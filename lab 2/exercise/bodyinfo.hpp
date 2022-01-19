22263ac3b8b723104a54526b9ec18eb6eeb5c447
TylerEpp52 <97622915+TylerEpp52@users.noreply.github.com>
2022-01-19 16:44:10 -0700

    Add files via upload

---
+++ lab 2/exercise/bodyinfo.hpp
@@ -0,0 +1,12 @@
+#ifndef BMI_HPP
+#define BMI_HPP
+
+#include <iostream>
+
+using namespace std;
+
+/*TODO 1: Create a struct "BodyInfo" with height and weight
+* ...
+*/
+
+#endif
---
+++ lab 2/exercise/computeBMI.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+#include "bodyinfo.hpp"
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+
+    if (argc < 3){
+        cout << "missing command line arguments" << endl;
+        return -1;
+    }
+
+    /* TODO 2: Collect height "inHeight" and weight "inWeight" from command line arguments
+    *float inHeight = ...
+    *float inWeight = ...
+    */
+
+    /* TODO 3: Assign data to BodyInfo variable
+    *BodyInfo newBody;
+    * ...
+    */
+
+    /* TODO 4: Compute BMI using only pointer access
+    * ...
+    */
+
+    /* TODO 5: Print height, weight, and bmi.
+    * ...
+    */
+    
+    return 0;
+}
